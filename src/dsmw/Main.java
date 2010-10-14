49b229768444431e7d52c2fac54afbcf6ef721d4
Khaled <kmobayed@gmail.com>
2010-10-14 11:24:19 +0200

    test 2

--- src/dsmw/Main.java
+++ src/dsmw/Main.java
@@ -28,23 +28,23 @@ public class Main {
         G.getVersion();
         G.getLog();
 
-        String page="http://en.wikipedia.org/w/";
-        Wikipedia W= new Wikipedia(page);
-        W.getPage();
-
-        String DBdirectory = args[0] ;
-        String ontoFile = "file:"+args[1];
-
-        Site MySite = new Site("Site1");
-
-        Jena J= new Jena(DBdirectory,ontoFile);
-
-        J.addSite(MySite);
-
-        J.listSites();
-        //System.out.println("===========");
-        //J.listStatements();
-        J.close();
+//        String page="http://en.wikipedia.org/w/";
+//        Wikipedia W= new Wikipedia(page);
+//        W.getPage();
+//
+//        String DBdirectory = args[0] ;
+//        String ontoFile = "file:"+args[1];
+//
+//        Site MySite = new Site("Site1");
+//
+//        Jena J= new Jena(DBdirectory,ontoFile);
+//
+//        J.addSite(MySite);
+//
+//        J.listSites();
+//        //System.out.println("===========");
+//        //J.listStatements();
+//        J.close();
     }
 
 }
