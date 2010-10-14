ed8484a7585d72e7727ab6d3055bbc2b68eb17b9
Khaled <kmobayed@gmail.com>
2010-10-14 11:24:00 +0200

    test

--- src/dsmw/Git.java
+++ src/dsmw/Git.java
@@ -5,10 +5,12 @@ package dsmw;
 import edu.nyu.cs.javagit.api.DotGit;
 import edu.nyu.cs.javagit.api.JavaGitConfiguration;
 import edu.nyu.cs.javagit.api.JavaGitException;
+import edu.nyu.cs.javagit.api.commands.GitLogOptions;
 import edu.nyu.cs.javagit.api.commands.GitLogResponse.Commit;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 /**
  *
  * @author klm
@@ -39,12 +41,39 @@ public class Git {
         File repositoryDirectory = new File("/Users/klm/code/cakephp");
         DotGit dotGit = DotGit.getInstance(repositoryDirectory);
 
-        System.out.println("Log size = "+dotGit.getLog().size());
+        System.out.println("Log size = "+dotGit.getLog().size())
+                ;
 
-        // Print commit messages of the current branch
-//        for (Commit c : dotGit.getLog()) {
-//            System.out.println(c.getMessage());
-//        }
+         //Print commit messages of the current branch
+        
+        //for (Commit c : dotGit.getLog()) {
+
+        GitLogOptions options=new GitLogOptions();
+        options.setOptOrderingReverse(true);
+        options.setOptLimitFirstParent(true);
+        options.setOptLimitFullHistory(true);
+        //options.set
+
+            Commit c= dotGit.getLog(options).get(0);
+            System.out.println(c.getMessage());
+            System.out.println(c.getAuthor());
+            System.out.println(c.getDateString());
+            System.out.println(c.getFiles());
+            System.out.println(c.getFilesChanged());
+            System.out.println(c.getLinesDeleted());
+            System.out.println(c.getLinesInserted());
+
+            if (c.getMergeDetails()!= null)
+            {
+                Iterator i = c.getMergeDetails().iterator();
+                while (i.hasNext())
+                {
+                    String MD = i.next().toString();
+                    System.out.println("Merge Detail ----"+MD);
+                }
+            }
+
+        //}
     }
 
 }
